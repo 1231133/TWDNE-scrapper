78a94b10ca81d7506bac89923c0728ee4f325c3b
PaintedCat <48541405+PaintedCat@users.noreply.github.com>
2020-10-20 20:15:35 +0300

    Update twdne.py
    Updated the name of the variables and the link to Github.

--- twdne.py
+++ twdne.py
@@ -3,17 +3,17 @@ import random
 import os
 pts=input('Print path to save waifu\n')
 i=int(input('Print nums of waifu\n'))
-temp1='https://www.thiswaifudoesnotexist.net/example-'
-temp2='.jpg'
+url='https://www.thiswaifudoesnotexist.net/example-'
+ext='.jpg'
 os.chdir(pts)
 for i in range(0,i):
     exnum=random.randint(1,99999)
     exnumstr=str(exnum)
-    flink=temp1+exnumstr+temp2
+    flink=url+exnumstr+ext
     file=exnumstr+'.jpg'
     r = requests.get(flink)
     with open(file, "wb") as code:
         code.write(r.content)
     print('Downloading {}... Completed!'.format(file))
-print('Programm by Alexsandr Uskov\nGithub:https://github.com/1231133/TWDNE-scrapper')
+print('Programm by Alexsandr Uskov\nGithub:https://github.com/PaintedCat/TWDNE-scrapper')
 
